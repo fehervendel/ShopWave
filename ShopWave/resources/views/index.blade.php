d82f52de33fc895e1e769a45b4ff69cf60e28344
fehervendel <fehervendel@gmail.com>
2024-03-12 20:54:40 +0100

    feature: add hero-section + change images

--- ShopWave/resources/views/index.blade.php
+++ ShopWave/resources/views/index.blade.php
@@ -2,14 +2,23 @@
 
 @section('content')
     
-    <h1>Main page</h1>
+    <section class="hero-section">
+        <div class="hero-content-left">
+            <h1 class="hero-title">GLOWSTEP-X1 MODEL</h1>
+            <p class="section-text">Discover the perfect comfort with our new range of footwear.<br>Step up your fashion game and embrace the essence of GLOWSTEP.<br>Find your perfect fit today and experience luxury for your feet.</p>
+            <button class="product-button">SEE PRODUCT</button>
+        </div>
+        <div class="hero-content-right">
+            <img src="/images/sneaker-1.png" class="hero-img" alt="Kép leírása">
+        </div>
+    </section>
 
     <section class="content-section">
         <div class="content-left">
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
         </div>
         <div class="content-right">
-            <img src="/images/nice.png" class="section-image" alt="Kép leírása">
+            <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
         </div>
     </section>
 
@@ -17,7 +26,7 @@
 
     <section class="content-section">
         <div class="content-left">
-            <img src="/images/nice.png" class="section-image" alt="Kép leírása">
+            <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
         </div>
         <div class="content-right">
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
@@ -29,13 +38,13 @@
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
         </div>
         <div class="content-right">
-            <img src="/images/nice.png" class="section-image" alt="Kép leírása">
+            <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
         </div>
     </section>
 
     <section class="content-section">
         <div class="content-left">
-            <img class="section-image" src="/images/nice.png" alt="Kép leírása">
+            <img class="section-image" src="/images/sneaker-1.png" alt="Kép leírása">
         </div>
         <div class="content-right">
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
