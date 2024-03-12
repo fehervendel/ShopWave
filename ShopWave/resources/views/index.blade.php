c2e395d9a9ffd22ed40810e43a035500a8d8ce24
fehervendel <fehervendel@gmail.com>
2024-03-12 15:03:33 +0100

    feature: add landing page

---
+++ ShopWave/resources/views/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layout')
+
+@section('content')
+    
+    <h1>Main page</h1>
+
+    <section class="content-section">
+        <div class="content-left">
+            <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
+        </div>
+        <div class="content-right">
+            <img src="/images/nice.png" class="section-image" alt="Kép leírása">
+        </div>
+    </section>
+
+    <h1>Valami szöveg</h1>
+
+    <section class="content-section">
+        <div class="content-left">
+            <img src="/images/nice.png" class="section-image" alt="Kép leírása">
+        </div>
+        <div class="content-right">
+            <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
+        </div>
+    </section>
+
+    <section class="content-section">
+        <div class="content-left">
+            <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
+        </div>
+        <div class="content-right">
+            <img src="/images/nice.png" class="section-image" alt="Kép leírása">
+        </div>
+    </section>
+
+    <section class="content-section">
+        <div class="content-left">
+            <img class="section-image" src="/images/nice.png" alt="Kép leírása">
+        </div>
+        <div class="content-right">
+            <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
+        </div>
+    </section>
+
+@endsection
