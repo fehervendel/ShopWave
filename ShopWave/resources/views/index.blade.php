3f23ea8a328ef63b60e2968df7c717900b449c82
fehervendel <fehervendel@gmail.com>
2024-03-13 13:05:44 +0100

    feature: add categories section

--- ShopWave/resources/views/index.blade.php
+++ ShopWave/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    
+                            {{-- Hero section --}}
     <section class="hero-section">
         <div class="hero-content-left">
             <h1 class="hero-title">GLOWSTEP-X1 MODEL</h1>
@@ -13,6 +13,34 @@
         </div>
     </section>
 
+                            {{-- Category section --}}
+    <section class="image-text-section">
+        <div class="image-container">
+            <div class="image-wrapper">
+                <img src="/images/sneaker-2.png" alt="Első kép">
+                <h1>Sneakers</h1>
+                <a>SHOP ></a>
+            </div>
+            <div class="image-wrapper">
+                <img src="/images/boot-1.png" alt="Második kép">
+                <h1>Boots</h1>
+                <a>SHOP ></a>
+            </div>
+            <div class="image-wrapper">
+                <img src="/images/high-heels-1.png" alt="Harmadik kép">
+                <h1>High heels</h1>
+                <a>SHOP ></a>
+            </div>
+        </div>
+    </section>
+                            {{-- Parallax section --}}
+    <div class="parallax">
+        <div class="parallax-background"></div>
+            <div class="parallax-content">
+                                    <!-- Tartalom -->
+            </div>
+    </div>                           
+                            {{-- Section --}}
     <section class="content-section">
         <div class="content-left">
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
@@ -21,9 +49,7 @@
             <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
         </div>
     </section>
-
-    <h1>Valami szöveg</h1>
-
+                            {{-- Section --}}
     <section class="content-section">
         <div class="content-left">
             <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
@@ -32,7 +58,7 @@
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
         </div>
     </section>
-
+                            {{-- Section --}}
     <section class="content-section">
         <div class="content-left">
             <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
@@ -41,7 +67,7 @@
             <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
         </div>
     </section>
-
+                            {{-- Section --}}
     <section class="content-section">
         <div class="content-left">
             <img class="section-image" src="/images/sneaker-1.png" alt="Kép leírása">
