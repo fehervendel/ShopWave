7a43f6f8c061d469262128f21d2c2cceb06c0c18
fehervendel <fehervendel@gmail.com>
2024-03-14 11:53:44 +0100

    feature: add classes to tages, finish 1 section

--- ShopWave/resources/views/index.blade.php
+++ ShopWave/resources/views/index.blade.php
@@ -47,12 +47,13 @@
             </div>
     </div>                           
                             {{-- Section --}}
-    <section class="content-section">
+    <section class="content-section gray">
         <div class="content-left">
-            <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices, orci sit amet auctor vulputate, dui odio venenatis purus, a imperdiet enim dolor maximus diam. Phasellus egestas urna vel augue tincidunt, vel fringilla dolor vehicula. Nunc rutrum risus erat. Ut a dui augue. Cras gravida nunc nec consectetur pretium. Nullam eleifend purus ornare semper cursus. Donec dignissim sem eget sapien eleifend, nec tempus ipsum molestie. Mauris urna tellus, sollicitudin non vestibulum eget, tristique non ligula.</p>
+            <h1 class="section-h1">LIPSTEP FINE-RED</h1>
+            <button class="product-button product-button-2">SEE PRODUCT</button>
         </div>
         <div class="content-right">
-            <img src="/images/sneaker-1.png" class="section-image" alt="Kép leírása">
+            <img src="/images/high-heels-2.png" class="section-image" id="high-heels-2" alt="Kép leírása">
         </div>
     </section>
                             {{-- Section --}}
