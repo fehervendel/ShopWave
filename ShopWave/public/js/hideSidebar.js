7e92cccccfdbffae7ebd4ae548a395caecdea23d
fehervendel <fehervendel@gmail.com>
2024-03-18 13:50:44 +0100

    feature: add js for sidebar

---
+++ ShopWave/public/js/hideSidebar.js
@@ -0,0 +1,4 @@
+function hideSidebar() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'none';
+}
---
+++ ShopWave/public/js/showSidebar.js
@@ -0,0 +1,4 @@
+function showSidebar() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'flex';
+}
