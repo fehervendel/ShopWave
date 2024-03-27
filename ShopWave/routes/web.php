50144ef4a994035c6186522c22dea1ecfe6557e1
fehervendel <fehervendel@gmail.com>
2024-03-27 17:33:58 +0100

    feature: add route

--- ShopWave/routes/web.php
+++ ShopWave/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('welcome');
+
+Route::get('/{category}', [ProductsController::class, 'index'])->where('category', 'sneaker|boots|high-hells')->name('products.index');
