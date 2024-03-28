11dba93b02c107b3d72b4fe1a464968fdd47f762
fehervendel <fehervendel@gmail.com>
2024-03-28 13:08:16 +0100

    feature: add categories index.blade.php, add zig-zag section

--- ShopWave/resources/views/products/index.blade.php
+++ ShopWave/resources/views/products/index.blade.php
@@ -2,14 +2,38 @@
 
 @section('content')
 <section class="category-hero">
-    <h1 class="category-hero-h1">SNEAKERS</h1>
+    <h1 class="category-hero-h1">{{$products[0]->category}}</h1>
 </section>
 
-<ul>
-    @foreach($products as $product)
-        <li>{{ $product->name }}</li>
-    @endforeach    
-</ul>   
+
+@foreach($products as $index => $product)
+    @if ($index % 2 == 0)
+        <section class="zig-zag">
+            <div class="image-side">
+                <img src="{{ $product->imageUrl }}">
+            </div>
+            <div class="text-side">
+                <h1>{{$product->name}}</h1>
+                <p class="orange">Some additional text that looks nice</p>
+                <p>Unique description for the product, that shows how insane and unique this product is. Doesn't matter how long text I add here, the text container will get the job done. I can add description for my model paste that text here.</p>
+                <button class="product-button zig-zag-button">SEE PRODUCT</button>
+            </div>
+        </section>
+    @else
+        <section class="zig-zag">
+            <div class="text-side">
+                <h1>{{$product->name}}</h1>
+                <p class="orange">Some additional text that looks nice</p>
+                <p>Unique description for the product, that shows how insane and unique this product is. Doesn't matter how long text I add here, the text container will get the job done. I can add description for my model paste that text here.</p>
+                <button class="product-button zig-zag-button">SEE PRODUCT</button>
+            </div>
+            <div class="image-side">
+                <img src="{{ $product->imageUrl }}">
+            </div>
+        </section>
+    @endif
+@endforeach
+
 
 
 <section class="image-text-section">
