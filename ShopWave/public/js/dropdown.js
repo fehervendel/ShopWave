abce4a050a3c1800db8e4aaf2f42401d5c8653b1
fehervendel <fehervendel@gmail.com>
2024-03-28 14:24:49 +0100

    feature: add phone dropdown event in dropdown.js

--- ShopWave/public/js/dropdown.js
+++ ShopWave/public/js/dropdown.js
@@ -11,11 +11,26 @@ document.addEventListener("DOMContentLoaded", function() {
     var categoryDropdown = document.getElementById("categoryDropdown");
     var categoryMenu = document.getElementById("categoryMenu");
 
+    var categoryDropdownPhone = document.getElementById("categoryDropdown-phone");
+    var categoryMenuPhone = document.getElementById("categoryMenu-phone");
+
     categoryDropdown.addEventListener("mouseover", function () {
         categoryMenu.style.display = "flex";
     });
 
     categoryDropdown.addEventListener("mouseleave", function() {
         categoryMenu.style.display = "none";
+    });
+
+    categoryDropdownPhone.addEventListener("click", function () {
+        if(categoryMenuPhone.style.display == "block") {
+            categoryMenuPhone.style.display = "none";
+        } else {
+            categoryMenuPhone.style.display = "block"
+        }
+    });
+
+    categoryDropdownPhone.addEventListener("onclick", function() {
+
     });
 });
