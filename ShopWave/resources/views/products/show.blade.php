491cc4e5743aa9819e0a03c56c2eb5b9e22100b6
fehervendel <fehervendel@gmail.com>
2024-04-08 15:20:37 +0200

    feature: add more elements to product show.blade.php

--- ShopWave/resources/views/products/show.blade.php
+++ ShopWave/resources/views/products/show.blade.php
@@ -1,6 +1,20 @@
 @extends('layout')
 
 @section('content')
-    <h1>Ennek a productnak az oldala:</h1>
-    <h1>{{ $product->name }}</h1>
+    <div class="item-section">
+
+        <div class="item-image-container">
+            <img class="item-image" src="{{ asset($product->imageUrl) }}">
+        </div>
+            
+        <div class="item-text-container">
+            <p class="item-category-p">{{ $product->category }}</p>
+            <h1 class="item-name">{{ $product->name }}</h1>
+            <p class="item-description">Product description which will come later from the database, need a few sentences here. Some more additional text that describes the current product.</p>
+            <p class="item-price">$ 599 (later from database)</p>
+            <a class="product-button">Add to cart</a>
+        </div>
+
+    </div>
+   
 @endsection
