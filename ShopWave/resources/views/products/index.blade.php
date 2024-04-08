a339804fd32165dce484c168bc3f5c4c792822d2
fehervendel <fehervendel@gmail.com>
2024-04-08 14:03:54 +0200

    feature: add links to buttons

--- ShopWave/resources/views/products/index.blade.php
+++ ShopWave/resources/views/products/index.blade.php
@@ -16,7 +16,7 @@
                 <h1>{{$product->name}}</h1>
                 <p class="orange">Some additional text that looks nice</p>
                 <p>Unique description for the product, that shows how insane and unique this product is. Doesn't matter how long text I add here, the text container will get the job done. I can add description for my model paste that text here.</p>
-                <button class="product-button zig-zag-button">SEE PRODUCT</button>
+                <a href="{{ route('products.show', $product) }}" class="product-button zig-zag-button">SEE PRODUCT</a>
             </div>
         </section>
     @else
@@ -25,7 +25,7 @@
                 <h1>{{$product->name}}</h1>
                 <p class="orange">Some additional text that looks nice</p>
                 <p>Unique description for the product, that shows how insane and unique this product is. Doesn't matter how long text I add here, the text container will get the job done. I can add description for my model paste that text here.</p>
-                <button class="product-button zig-zag-button">SEE PRODUCT</button>
+                <a href="{{ route('products.show', $product) }}" class="product-button zig-zag-button">SEE PRODUCT</a>
             </div>
             <div class="image-side">
                 <img src="{{ $product->imageUrl }}">
