2a2a0dd7fa0d79aae3cec6a247cef44d451a357c
fehervendel <fehervendel@gmail.com>
2024-04-16 10:16:59 +0200

    feature: add sizes and read product data from object

--- ShopWave/resources/views/products/show.blade.php
+++ ShopWave/resources/views/products/show.blade.php
@@ -6,22 +6,28 @@
         <div class="item-image-container">
             <img class="item-image" src="{{ asset($product->imageUrl) }}">
         </div>
-@php
-    $selectedSize = $product->size;
-@endphp
+        @php
+            $selectedSize = $product->size;
+        @endphp
         <div class="item-text-container">
             <p class="item-category-p">{{ $product->category }}</p>
             <h1 class="item-name">{{ $product->name }}</h1>
-            <p class="item-description">Product description which will come later from the database, need a few sentences here. Some more additional text that describes the current product.</p>
+            <p class="item-description">Product description which will come later from the database, need a few
+                sentences here. Some more additional text that describes the current product.</p>
             <p class="item-price">{{ $product->price }} $</p>
             <div>
                 <ul class="sizes-list">
                     @foreach($sizes as $size)
-                        <li id="size_{{ $size }}" class="size @if($size === $selectedSize) activeSize @endif">{{ $size }}</li>
+                        <li id="size_{{ $size }}"
+                            class="size @if($size === $selectedSize) activeSize @endif">{{ $size }}</li>
                     @endforeach
                 </ul>
             </div>
-            <a class="product-button item-button">Add to cart</a>
+            <button type="submit" data-link="{{ route('cart.store', ['id' => $product->id]) }}"
+                    class="product-button item-button">Add to cart
+            </button>
+
+
         </div>
 
     </div>
