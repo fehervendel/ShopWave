e2103cab49dbfd2c24e0766a1671045cf27b4e1a
fehervendel <fehervendel@gmail.com>
2024-04-25 13:00:48 +0200

    feature: add js to remove element from DOM and calculate new prices without reloading

--- ShopWave/public/js/sendForm.js
+++ ShopWave/public/js/sendForm.js
@@ -17,5 +17,45 @@ $( document ).ready(function() {
         });
         return false;
     });
+
+    $('.cart-item-remove').on('click', function (e) {
+        e.preventDefault();
+
+        let link = $(this).attr('data-link');
+        let cartItem = $(this).closest('.cart-item');
+
+        let itemPriceText = cartItem.find('.cart-item-price').text();
+        let itemPrice = parseFloat(itemPriceText.replace('$', '').trim());
+
+        let totalPriceText = $('#totalPrice').text();
+        let totalPrice = parseFloat(totalPriceText.replace('$', '').trim());
+
+        /*let vatPriceText = $('#vatPrice').text();
+        let vatPrice = parseFloat(vatPriceText.replace('$', '').trim());
+
+        let grandTotalPriceText = $('#grandTotalPrice').text();
+        let grandTotalPrice = parseFloat(grandTotalPriceText.replace('$', '').trim());*/
+
+        let newTotalPrice = totalPrice - itemPrice.toFixed(2);
+
+        $.ajax({
+            url: link,
+            method: 'DELETE',
+            data: {
+                _token : csrf_token,
+            }
+        }).done(function(json) {
+            if(json.response === true) {
+                cartItem.remove();
+                $('#totalPrice').text(`$ ${newTotalPrice}`);
+                $('#vatPrice').text(`$ ${(newTotalPrice * 0.27).toFixed(2)}`);
+                $('#grandTotalPrice').text(`$ ${(newTotalPrice + 20)}`);
+                toastr.success('Item removed successfully!', 'Success');
+            } else {
+                toastr.error('Failed to remove item.', 'Error');
+            }
+        });
+        return false;
+    })
 });
 
